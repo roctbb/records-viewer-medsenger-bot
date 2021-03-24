50f3624943df7f96e81c034532ae98503b4104d4
roctbb <roctbb@gmail.com>
2021-03-24 10:17:44 +0300

    remove wkhtmltopdf

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo pip3 install -r requirements.txt
-sudo aplt-get install python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info python3-setuptools python3-wheel build-essential python3-dev
+sudo apt-get install python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info python3-setuptools python3-wheel build-essential python3-dev
 sudo cp agents_records.conf /etc/supervisor/conf.d/
 sudo cp agents_records_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
