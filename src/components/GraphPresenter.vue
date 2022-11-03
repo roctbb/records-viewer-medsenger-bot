0656db7dd6261fa8bb57ea9e8ae0a9013371ecaf
Nadia Laban <labannadia5@gmail.com>
2022-11-03 23:09:07 +0300

    fix

--- src/components/GraphPresenter.vue
+++ src/components/GraphPresenter.vue
@@ -189,8 +189,8 @@ export default {
       this.data.filter(graph => this.text_categories.includes(graph.category.name)).forEach(graph => {
         records = records.concat(graph.values.map(record => {
           record.category_info = graph.category
-          record.formatted_date = moment(record.timestamp).format('hh:mm:ss DD.MM.YYYY')
-          record.date = moment(record.timestamp).format('DD.MM.YYYY')
+          record.formatted_date = moment((record.timestamp + this.offset()) * 1000).format('hh:mm:ss DD.MM.YYYY')
+          record.date = moment((record.timestamp + this.offset()) * 1000).format('DD.MM.YYYY')
           return record
         }))
       })
