d74cd8d4d0615e6025ae6e85c2bfd31d8564505f
Stephan Zhdanov <55328925+ret7020@users.noreply.github.com>
2023-01-09 15:15:53 +0300

    PEP 8 fix
    Hello!

--- managers/ContractsManager.py
+++ managers/ContractsManager.py
@@ -15,7 +15,8 @@ class ContractManager(Manager):
             self.db.session.add(contract)
 
         contract.is_active = True
-        contract.agent_token = self.medsenger_api.get_agent_token(contract_id).get('agent_token')
+        contract.agent_token = self.medsenger_api.get_agent_token(
+            contract_id).get('agent_token')
 
         self.__commit__()
 
@@ -26,7 +27,8 @@ class ContractManager(Manager):
             contract = Contract.query.filter_by(id=contract_id).first()
 
             if not contract:
-                raise Exception("No contract_id = {} found".format(contract_id))
+                raise Exception(
+                    "No contract_id = {} found".format(contract_id))
 
             contract.is_active = False
             self.__commit__()
@@ -45,23 +47,6 @@ class ContractManager(Manager):
         contract = Contract.query.filter_by(id=contract_id).first()
 
         return not contract
-
-    def add_params(self, contract_id, params):
-        try:
-            contract = Contract.query.filter_by(id=contract_id).first()
-
-            if not contract:
-                raise Exception("No contract_id = {} found".format(contract_id))
-
-            if not contract.params:
-                contract.params = params
-            else:
-                contract.params.update(params)
-
-            self.__commit__()
-            return contract
-        except Exception as e:
-            log(e)
 
     def get_active_ids(self):
         return [contract.id for contract in Contract.query.filter_by(is_active=True).all()]
