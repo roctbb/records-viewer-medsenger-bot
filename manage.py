5b31ea135a7f497d53a237659adc754aafb3fcf9
roctbb <roctbb@gmail.com>
2023-02-06 16:50:01 +0300

    remove flask script

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from models import db
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 
 from config import *
 
@@ -12,9 +11,3 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_string
 db.init_app(app)
 
 migrate = Migrate(app, db)
-
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
-
-if __name__ == '__main__':
-    manager.run()
