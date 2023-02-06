6725add55864f1d1c93e49bc3329468fa6d0c28a
roctbb <roctbb@gmail.com>
2023-02-06 16:53:38 +0300

    remove flask script

--- restore_backup.sh
+++ restore_backup.sh
@@ -1,2 +1,2 @@
 unzip records-viewer.zip
-pg_sql -U postgres -W -d records-viewer-medsenger-bot -h 127.0.0.1 < records-viewer_bot.sql
+psql -U postgres -W -d records-viewer-medsenger-bot -h 127.0.0.1 < records-viewer_bot.sql
