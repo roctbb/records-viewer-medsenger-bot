ed87231792651014ddeb54a5799e960010ca75d2
roctbb <roctbb@gmail.com>
2023-05-12 20:41:17 +0300

    add compression

--- manage.py
+++ manage.py
@@ -2,12 +2,14 @@ from flask import Flask
 from models import db
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_compress import Compress
 
 from config import *
 
 app = Flask(__name__)
 
 CORS(app)
+Compress(app)
 
 db_string = "postgresql://{}:{}@{}:{}/{}".format(DB_LOGIN, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_string
