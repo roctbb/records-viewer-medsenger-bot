503c9edb04553f1ae5fd3e4abe4f667ee9a80709
roctbb <roctbb@gmail.com>
2023-12-05 18:07:26 +0300

    add show_warning for additions

--- src/components/views/formolized-report/parts/ActionField.vue
+++ src/components/views/formolized-report/parts/ActionField.vue
@@ -2,7 +2,7 @@
     <div v-if="field" style="margin-top: 15px; margin-bottom: 5px;">
         <div class="row" style="margin: -5px">
             <div class="col-md-5">
-                <a class="btn btn-primary btn-block" :href="get_url" v-if="!field.params">{{ field.text }}</a>
+                <a class="btn btn-primary btn-block" target="_blank" :href="get_url" v-if="!field.params">{{ field.text }}</a>
                 <button class="btn btn-primary btn-block" @click="send_request()" v-else>{{ field.text }}</button>
             </div>
             <div class="col-md-7" v-if="field.params">
