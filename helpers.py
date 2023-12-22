4ce36f87fc8b3aad8a81f89e7339dd90fb5379da
Nadia Laban <labannadia5@gmail.com>
2023-12-22 17:01:53 +0300

    fix

--- helpers.py
+++ helpers.py
@@ -12,7 +12,7 @@ from medsenger_api import AgentApiClient
 from config import *
 
 medsenger_api = AgentApiClient(APP_KEY, MAIN_HOST, AGENT_ID, API_DEBUG, USE_GRPC, GRPC_HOST, sentry_dsn=SENTRY)
-text_categories = ['symptom', 'medicine', 'patient_comment', 'information']
+text_categories = ['symptom', 'medicine', 'patient_comment', 'information', 'side_effect']
 
 
 def gts():
