8ffcd7219f75b1009edfac7b66ed0a59faa3b7b9
Nadia Laban <labannadia5@gmail.com>
2024-01-15 18:01:11 +0300

    style

--- src/components/views/formalized-report/parts/ActionField.vue
+++ src/components/views/formalized-report/parts/ActionField.vue
@@ -2,8 +2,8 @@
     <div v-if="field" style="margin-top: 15px; margin-bottom: 5px;">
         <div class="row" style="margin: -5px">
             <div class="col-md-5">
-                <a class="btn btn-primary btn-block" :target="link_target" :href="get_url" v-if="!field.params">{{ field.text }}</a>
-                <button class="btn btn-primary btn-block" @click="send_request()" v-else>{{ field.text }}</button>
+                <a class="btn btn-success" :target="link_target" :href="get_url" v-if="!field.params">{{ field.text }}</a>
+                <button class="btn btn-success" @click="send_request()" v-else>{{ field.text }}</button>
             </div>
             <div class="col-md-7" v-if="field.params">
                 <div v-for="param in field.params">
