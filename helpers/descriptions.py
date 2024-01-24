06523f0315db5137b1820472278fddc6e06deddb
Nadia Laban <labannadia5@gmail.com>
2024-01-24 17:39:15 +0300

    fix

--- helpers/descriptions.py
+++ helpers/descriptions.py
@@ -1,2 +1,16 @@
 
 
+def get_graph_id(category_id):
+    ids = {
+        '24': 6,
+        '2': 3,
+        '1': 3,
+        '-1': 1,
+        '79': 11,
+        '20': 5
+    }
+
+    if category_id in ids:
+        return '.group_page_with_args', ids[category_id]
+
+    return '.single_graph_page', category_id
